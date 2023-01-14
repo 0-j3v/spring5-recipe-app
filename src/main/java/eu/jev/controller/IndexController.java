8cd12dccbdf96811a2d0e0033c6626f3e42856a2
J3v <37551611+JevP@users.noreply.github.com>
2023-01-14 15:53:45 +0200

    Adding Query Methods Examples

--- src/main/java/eu/jev/controller/IndexController.java
+++ src/main/java/eu/jev/controller/IndexController.java
@@ -1,13 +1,31 @@
 package eu.jev.controller;
 
+import eu.jev.domain.Category;
+import eu.jev.domain.UnitOfMeasure;
+import eu.jev.repository.CategoryRepository;
+import eu.jev.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Teaspoon");
+        System.out.println("Cat ID is:" + categoryOptional.get().getId());
+        System.out.println("UOM ID is:" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/eu/jev/repository/UnitOfMeasureRepository.java
+++ src/main/java/eu/jev/repository/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package eu.jev.repository;
 import eu.jev.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByUom(String uom);
 }
