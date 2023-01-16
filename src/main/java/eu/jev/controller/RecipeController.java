ea3496c09860c6d81ab604c9abf25b647e15597b
J3v <37551611+JevP@users.noreply.github.com>
2023-01-16 19:33:08 +0200

    Added pages and test for display of Recipe

---
+++ src/main/java/eu/jev/controller/RecipeController.java
@@ -0,0 +1,25 @@
+package eu.jev.controller;
+
+import eu.jev.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class RecipeController {
+
+    RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
+        return "recipe/show";
+    }
+}
