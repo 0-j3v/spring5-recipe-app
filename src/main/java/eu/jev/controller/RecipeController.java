06cb4eaac9f5a8b6b5efa56608f2d34081a3cf7f
J3v <37551611+JevP@users.noreply.github.com>
2023-01-16 21:27:33 +0200

    Added create new Recipe form and controller methods

--- src/main/java/eu/jev/controller/RecipeController.java
+++ src/main/java/eu/jev/controller/RecipeController.java
@@ -1,10 +1,14 @@
 package eu.jev.controller;
 
+import eu.jev.command.RecipeCommand;
+import eu.jev.domain.Recipe;
 import eu.jev.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -21,5 +25,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe/")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
